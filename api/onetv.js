122949dcf00fe3d437c8824c683219a96d790ebb
RJMBTS <rjmbts@gmail.com>
2025-11-04 00:03:59 +0530

    Update onetv.js

--- api/onetv.js
+++ api/onetv.js
@@ -5,7 +5,7 @@ let lastFetchTime = 0;
 
 export default async function handler(req, res) {
   const CACHE_DURATION = 1000 * 60 * 15; // 15 minutes
-  const fileUrl = "https://raw.githubusercontent.com/RJMBTS/Aupl/refs/heads/main/Master.m3u";
+  const fileUrl = "https://raw.githubusercontent.com/bugsfreeweb/LiveTVCollector/refs/heads/main/LiveTV/India/LiveTV.m3u";
 
   // Serve from cache if still valid
   if (cachedData && Date.now() - lastFetchTime < CACHE_DURATION) {
